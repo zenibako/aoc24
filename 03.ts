d0c0c170e5544cbfdeae2aa211f1c4f1b2e98f8a
Chandler Anderson <chandler.anderson@odaseva.com>
2024-12-07 17:19:28 +0100

    Day 3 Part 2 solution

--- 03.ts
+++ 03.ts
@@ -1,14 +1,28 @@
 import fs from 'fs'
 
 const INPUT = fs.readFileSync('03_input.txt', 'utf8')
-const pattern = /mul\([0-9]+,[0-9]+\)/g
-const muls = INPUT.match(pattern)
+const pattern = /(mul\([0-9]+,[0-9]+\))|(do\(\))|(don't\(\))/g
+const matches = INPUT.match(pattern)
 
-const total = muls.reduce((acc: number, mul: string) => {
-  console.log(mul)
-  const numbers = mul.substring(mul.search('mul') + 4, mul.length-1)
-  const [a, b] = numbers.split(',')
-  return acc + (parseInt(a) * parseInt(b))
+let isDo = true
+let partTwoTotal = 0
+const total = matches.reduce((acc: number, match: string) => {
+  console.log(match)
+  if (match.startsWith('mul')) {
+    const numbers = match.substring(match.search('mul') + 4, match.length - 1)
+    const [a, b] = numbers.split(',')
+    const amount = (parseInt(a) * parseInt(b))
+    if (isDo) {
+      partTwoTotal += amount
+    }
+    return acc + amount
+  } else if (match.startsWith('don\'t')) {
+    isDo = false
+  } else if (match.startsWith('do')) {
+    isDo = true
+  }
+  return acc
 }, 0)
 
 console.log(`Part 1: ${total}`)
+console.log(`Part 2: ${partTwoTotal}`)
