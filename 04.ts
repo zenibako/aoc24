7f1d3005970b1c8c2d61147e42a31ca0625557ac
Chandler Anderson <chandler.anderson@odaseva.com>
2024-12-07 20:20:51 -0500

    Day 4 full solution

--- 04.ts
+++ 04.ts
@@ -1,7 +1,17 @@
-
 import fs from 'fs'
 
 const INPUT = fs.readFileSync('04_input.txt', 'utf8')
+const POSSIBLE_MATCHES = {
+  LR: true,
+  RL: true,
+  TB: true,
+  BT: true,
+  TLBR: true,
+  TRBL: true,
+  BRTL: true,
+  BLTR: true
+}
+
 
 const rows = INPUT.split('\n')
 
@@ -25,33 +35,40 @@ for (let y = 0; y < rows.length; y++) {
   }
 }
 
-const matchCount = letterIndexes.X.reduce((acc, [ x, y ]) => {
-  const possibleMatches = {
-    LR: true,
-    RL: true,
-    TB: true,
-    BT: true,
-    LTBR: true,
-    RTBL: true,
-    RBTL: true,
-    LBTR: true
-  }
+const matchesLetterAt = (letter: string, x: number, y: number) => rows[y]?.charAt(x) === letter
 
+const partOneMatches = letterIndexes.X.reduce((acc, [ x, y ]) => {
+  const isDirection = { ...POSSIBLE_MATCHES }
   for (let i = 1; i < 4; i++) {
-    const matchesNextLetterAt = (x: number, y: number) => (Object.keys(letterIndexes)[i] === rows[y]?.charAt(x))
-    possibleMatches.LR    &&= matchesNextLetterAt( x + i,  y     )
-    possibleMatches.RL    &&= matchesNextLetterAt( x - i,  y     )
-    possibleMatches.TB    &&= matchesNextLetterAt( x,      y - i )
-    possibleMatches.BT    &&= matchesNextLetterAt( x,      y + i )
-    possibleMatches.LTBR  &&= matchesNextLetterAt( x + i,  y - i )
-    possibleMatches.RTBL  &&= matchesNextLetterAt( x - i,  y - i )
-    possibleMatches.RBTL  &&= matchesNextLetterAt( x - i,  y + i )
-    possibleMatches.LBTR  &&= matchesNextLetterAt( x + i,  y + i )
+    const nextLetter = Object.keys(letterIndexes)[i]
+    isDirection.LR    &&= matchesLetterAt( nextLetter, x + i,  y     )
+    isDirection.RL    &&= matchesLetterAt( nextLetter, x - i,  y     )
+    isDirection.TB    &&= matchesLetterAt( nextLetter, x,      y - i )
+    isDirection.BT    &&= matchesLetterAt( nextLetter, x,      y + i )
+    isDirection.TLBR  &&= matchesLetterAt( nextLetter, x + i,  y - i )
+    isDirection.TRBL  &&= matchesLetterAt( nextLetter, x - i,  y - i )
+    isDirection.BRTL  &&= matchesLetterAt( nextLetter, x - i,  y + i )
+    isDirection.BLTR  &&= matchesLetterAt( nextLetter, x + i,  y + i )
   }
-  const actualMatches = Object.entries(possibleMatches)
+  const actualMatches = Object.entries(isDirection)
     .filter(([,isMatch]) => isMatch) 
     .map(([direction]) => direction)
   return acc + actualMatches.length
 }, 0)
 
-console.log(`Part 1: ${matchCount}`)
+const partTwoMatches = letterIndexes.A.reduce((acc, [ x, y ]) => {
+  const isDirection = { ...POSSIBLE_MATCHES }
+  isDirection.TLBR  = matchesLetterAt( 'M', x - 1,  y + 1 ) && matchesLetterAt( 'S', x + 1, y - 1 )
+  isDirection.TRBL  = matchesLetterAt( 'M', x + 1,  y + 1 ) && matchesLetterAt( 'S', x - 1, y - 1 )
+  isDirection.BRTL  = matchesLetterAt( 'M', x + 1,  y - 1 ) && matchesLetterAt( 'S', x - 1, y + 1 )
+  isDirection.BLTR  = matchesLetterAt( 'M', x - 1,  y - 1 ) && matchesLetterAt( 'S', x + 1, y + 1 )
+  
+  if ((isDirection.BLTR || isDirection.TRBL) && (isDirection.TLBR || isDirection.BRTL)) {
+    acc++
+  }
+  
+  return acc
+}, 0)
+
+console.log(`Part 1: ${partOneMatches}`)
+console.log(`Part 2: ${partTwoMatches}`)
